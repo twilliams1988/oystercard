f743f7b2ba3154802295d7d25b3b80c0c16ac105
sus111 <sgraham535@gmail.com>
2016-07-11 15:13:33 +0100

    add oystercard class

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,15 @@
+require 'oystercard'
+
 describe OysterCard do
+
+  subject(:oystercard) { described_class.new }
+
+  describe '#initialize' do
+
+    it 'has an initial balance of 0' do
+      expect(subject.balance).to eq 0
+    end
+
+  end
 
 end
